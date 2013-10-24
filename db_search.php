b099c995b588a742e188d93628e916df734c32ce
langos94 <minhlong.langos@gmail.com>
2013-10-24 23:37:41 +0100

    updating db_search.php

--- db_search.php
+++ db_search.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<Title>Database Search</Title>
+<Title>Search Form</Title>
 <style type="text/css">
     body { background-color: #fff; border-top: solid 10px #000;
         color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -16,37 +16,69 @@
 </style>
 </head>
 <body>
-<h1>Search here!</h1>
-<p>Fill in search criteria, then click <strong>Submit</strong> to search.</p>
-<form method="post" action="db_search.php" enctype="multipart/form-data" >
-	Name <input type="text" name="name" id="name"/></br>
-	Email <input type="text" name="email" id="email/></br>
-	<input type="submit" name="submit" value="Submit" />
+<h1>Register here!</h1>
+<p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+<form method="post" action="index.php" enctype="multipart/form-data" >
+      Name  <input type="text" name="name" id="name"/></br>
+      Email <input type="text" name="email" id="email"/></br>
+      <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
-	// DB connection info
-	$host = "eu-cdbr-azure-west-b.cloudapp.net";
-	$user = "bae74695f9d00f";
-	$pwd = "1fe84b0f";
-	$db = "minhlonAxoP7zDDA";
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+    $host = "eu-cdbr-azure-west-b.cloudapp.net";
+    $user = "bae74695f9d00f";
+    $pwd = "1fe84b0f";
+    $db = "minhlonAxoP7zDDA";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
 
-	// Connect to database.
-	try {
-		$conn = new PDO("mysql:host=$host;$dbname=$db", $user, $pwd);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch(Exception $e) {
-		die(var_dump($e);
-	}
-	if(!empty($_POST)) {
-	try {
-       		$name = $_GET['name'];
-        	$email = $_GET['email'];
-		// Search database for data
-	}
-	catch(Exception e) {
-		die(var_dump($e);
-	}
+    // Insert registration info
+    if(!empty($_POST)) {
+    try {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $date = date("Y-m-d");
+        // Insert data
+        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+                   VALUES (?,?,?)";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(1, $name);
+        $stmt->bindValue(2, $email);
+        $stmt->bindValue(3, $date);
+        $stmt->execute();
+    }
+    catch(Exception $e) {
+        die(var_dump($e));
+    }
+    echo "<h3>Your're registered!</h3>";
+    }
+    // Retrieve data
+    $sql_select = "SELECT * FROM registration_tbl";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+    if(count($registrants) > 0) {
+        echo "<h2>People who are registered:</h2>";
+        echo "<table>";
+        echo "<tr><th>Name</th>";
+        echo "<th>Email</th>";
+        echo "<th>Date</th></tr>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['name']."</td>";
+            echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['date']."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>No one is currently registered.</h3>";
+    }
 ?>
 </body>
 </html>
