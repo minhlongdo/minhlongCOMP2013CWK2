f0477b7e507b072a68967cb340ae678b93b113e3
Minh-Long Do <minhlong94@yahoo.de>
2013-10-25 14:45:17 +0100

    Fixed typo in index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 	$company = $_POST['company'];
         $date = date("Y-m-d");
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, Company_name, email, date) 
+        $sql_insert = "INSERT INTO registration_tbl (name, Company_Name, email, date) 
                    VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
