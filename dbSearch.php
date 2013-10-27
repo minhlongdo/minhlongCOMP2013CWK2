b5c42aa046cc6b605fb1b0f7e4f6962db959e7d4
langos94 <minhlong.langos@gmail.com>
2013-10-27 15:39:21 +0000

    Edit dbSearch.php

--- dbSearch.php
+++ dbSearch.php
@@ -46,43 +46,43 @@
         $name = $_POST['name'];
         $email = $_POST['email'];
 	$company = $_POST['company'];
-        $date = date("Y-m-d");
+        //$date = date("Y-m-d");
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, Company_Name, email, date) 
-                   VALUES (?,?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $company);
-	$stmt->bindValue(3, $email);
-        $stmt->bindValue(4, $date);
-        $stmt->execute();
+        // $sql_insert = "INSERT INTO registration_tbl (name, Company_Name, email, date) 
+        //           VALUES (?,?,?,?)";
+        //$stmt = $conn->prepare($sql_insert);
+        //$stmt->bindValue(1, $name);
+        //$stmt->bindValue(2, $company);
+	//$stmt->bindValue(3, $email);
+        //$stmt->bindValue(4, $date);
+        //$stmt->execute();
     }
     catch(Exception $e) {
         die(var_dump($e));
     }
-    echo "<h3>Your're registered!</h3>";
+    //echo "<h3>Your're registered!</h3>";
     }
     // Retrieve data
-    $sql_select = "SELECT * FROM registration_tbl";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    if(count($registrants) > 0) {
-        echo "<h2>People who are registered:</h2>";
-        echo "<table>";
-        echo "<tr><th>Name</th>";
-	echo "<th>Company name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Date</th></tr>";
-        foreach($registrants as $registrant) {
-            echo "<tr><td>".$registrant['name']."</td>";
-	    echo "<td>".$registrant['Company_Name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "<h3>No one is currently registered.</h3>";
-    }
+    //$sql_select = "SELECT * FROM registration_tbl";
+    //$stmt = $conn->query($sql_select);
+    //$registrants = $stmt->fetchAll(); 
+    //if(count($registrants) > 0) {
+    //    echo "<h2>People who are registered:</h2>";
+    //    echo "<table>";
+    //    echo "<tr><th>Name</th>";
+	//echo "<th>Company name</th>";
+        //echo "<th>Email</th>";
+        //echo "<th>Date</th></tr>";
+        //foreach($registrants as $registrant) {
+         //   echo "<tr><td>".$registrant['name']."</td>";
+	  //  echo "<td>".$registrant['Company_Name']."</td>";
+           // echo "<td>".$registrant['email']."</td>";
+           // echo "<td>".$registrant['date']."</td></tr>";
+       // }
+       // echo "</table>";
+   // } else {
+       // echo "<h3>No one is currently registered.</h3>";
+    //}
 ?>
 </body>
 </html>
