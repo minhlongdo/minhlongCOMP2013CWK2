dc5699d0c50af1612b258a1ac202ba2450de9398
langos94 <minhlong.langos@gmail.com>
2013-10-27 16:12:53 +0000

    Final version of CWK 2 of System Engineering 1 (COMP2013).

--- dbSearch.php
+++ dbSearch.php
@@ -46,39 +46,17 @@
         $name = $_POST['name'];
         $email = $_POST['email'];
 	$company = $_POST['company'];
-
-	// Retrieve data
-	//$sql_select = "SELECT * FROM registration_tbl WHERE name = '$name' or email = '$email' or Company_name = '$company'";
-	//$stmt = $conn->query($sql_select);
-	//$registrants = $stmt->fetchAll();
-	//if(count(registrants) > 0) {
-//		echo "Found people.";
-//	} else {
-//		echo "No search result."
-//	}
-
-        //$date = date("Y-m-d");
-        // Insert data
-        // $sql_insert = "INSERT INTO registration_tbl (name, Company_Name, email, date) 
-        //           VALUES (?,?,?,?)";
-        //$stmt = $conn->prepare($sql_insert);
-        //$stmt->bindValue(1, $name);
-        //$stmt->bindValue(2, $company);
-	//$stmt->bindValue(3, $email);
-        //$stmt->bindValue(4, $date);
-        //$stmt->execute();
     }
     catch(Exception $e) {
         die(var_dump($e));
     }
-    //echo "<h3>Your're registered!</h3>";
     }
     // Retrieve data
     $sql_select = "SELECT * FROM registration_tbl WHERE name = '$name' or email = '$email' or company_name = '$company'";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
     if(count($registrants) > 0) {
-        echo "<h2>People who are registered:</h2>";
+        echo "<h2>Search result:</h2>";
         echo "<table>";
         echo "<tr><th>Name</th>";
 	echo "<th>Company name</th>";
